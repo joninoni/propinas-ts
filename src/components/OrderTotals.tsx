7f9fc392abd9639f0ec8e450be18bcb3bbda569f
joninoni <jonibeltranhayer@gmail.com>
2024-03-30 14:25:36 -0600

    sumo el subtotal de las ordenes con la funcion subTotalAmount

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+    order : OrderItem[] 
+}
+
+
+const OrderTotals = ({order} : OrderTotalsProps) => {
+
+    const subTotalAmount= useMemo( ()=> order.reduce( (total,item) => total + (item.price * item.quantity) ,0),[order])
+
+    return (
+        <>
+            <div className=" space-y-3">
+                <h2 className="font-black text-2xl">Totales Y Propina</h2>
+
+                <p>Subototal a pagar : {""}
+                    <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
+                </p>
+
+                <p>Propina {""}
+                    <span className="font-bold">$0</span>
+                </p>
+
+                <p>Total a pagar {""}
+                    <span className="font-bold">$0</span>
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default OrderTotals
