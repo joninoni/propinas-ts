ee5d06ddedf657b01fc36c023193efe6ca4be8a0
joninoni <jonibeltranhayer@gmail.com>
2024-03-29 18:03:09 -0600

    construyo un nuevo arreglo que evite los duplicados y que solo incremente la cantidad

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,8 +5,19 @@ export default function useOrder(){
     const [order,setOrder] = useState<OrderItem[]>([])//el arreglo de la orden
 
     const addItem = (item : MenuItem) => {
-        const newItem: OrderItem = {...item, quantity:1}
-        setOrder([...order,newItem])
+        //verifica si existe
+        const existItem = order.find( orderItem => orderItem.id === item.id)
+
+        if(existItem){
+            const updateOrder = order.map( orderItem => orderItem.id === item.id ? {...orderItem,quantity:orderItem.quantity + 1} : orderItem)
+            setOrder(updateOrder)
+        }
+        else{
+            const newItem: OrderItem = {...item, quantity:1}
+            setOrder([...order,newItem])
+        }
+        
+      
     }
     return{
         addItem
